4763cc27b5774f20057837938226828e33d38041
LEE MYOHUI <zr.colaaa@gmail.com>
2026-02-09 23:50:39 +0900

    [해시] 의상 (42578)

---
+++ src/main/java/me/myohui/level1/P42578_의상.java
@@ -0,0 +1,33 @@
+package me.myohui.level1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>의상</p>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42578
+ * */
+public class P42578_의상 {
+    public static void main(String[] args) {
+        String[][] clothes = {
+            {"yellow_hat", "headgear"},
+            {"blue_sunglasses", "eyewear"},
+            {"green_turban", "headgear"}
+        };
+        System.out.println(solution(clothes));
+    }
+
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] s : clothes) {
+            map.put(s[1], map.getOrDefault(s[1], 1) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            answer *= map.get(key);
+        }
+
+        return answer - 1;
+    }
+}
